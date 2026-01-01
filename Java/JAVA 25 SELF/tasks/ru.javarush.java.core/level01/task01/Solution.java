aab956796e9fbef1a12438882fd64dcfc5bd89e8
Altavista360 <github@altavista360.email>
2026-01-01 21:13:18 +0300

    Create Solution.java

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task01/Solution.java
@@ -0,0 +1,31 @@
+// Первая команда 😎
+
+/*
+Напишите программу на Java, которая выводит на экран фразу: "Круто быть программистом!😎"
+*/
+
+/*
+package ru.javarush.java.core.level01.task01;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Выводим на экран фразу "Круто быть программистом!😎"
+
+    }
+}
+*/
+
+package ru.javarush.java.core.level01.task01;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        // Выводим на экран фразу "Круто быть программистом!😎"
+        System.out.println("Круто быть программистом!😎");
+    }
+}
+
+// https://javarush.com/tasks/ru.javarush.java.core.level01.task01
