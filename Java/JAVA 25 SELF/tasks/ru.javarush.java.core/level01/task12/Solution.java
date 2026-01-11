8ac04af0375485461e6a52bbfdeeeeb3fcdc8eac
Altavista360 <github@altavista360.email>
2026-01-11 09:22:18 +0300

    Create Solution.java

---
+++ Java/JAVA 25 SELF/tasks/ru.javarush.java.core/level01/task12/Solution.java
@@ -0,0 +1,61 @@
+// Название (JavaRush): Исправление ошибок 🐛
+// Title (CodeGym): Fixing bugs 🐛
+
+/*
+Описание:
+Вам дали код, который не компилируется. Найдите и закомментируйте все строки, которые вызывают ошибки или выводят что-то в консоль. Программа должна запускаться без проблем и не выводить ничего в консоль.
+*/
+
+/*
+Требования:
+• Все найденные строки с ошибками компиляции должны быть закомментированы с помощью //.
+• В программе запрещено вносить любые другие изменения, кроме добавления комментариев для закомментирования ошибочных строк.
+• После внесения изменений программа должна успешно компилироваться без ошибок.
+• Программа не должна выводить ничего в консоль после исправления.
+*/
+
+/*
+// TODO:
+package ru.javarush.java.core.level01.task12;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int number = 10;
+        int number = 20;
+
+        int uninitialized;
+        System.out.println(uninitialized);
+
+        String text = 123;
+
+        System.out.println(undeclaredVariable);
+
+        System.out.println(number);
+    }
+}
+*/
+
+package ru.javarush.java.core.level01.task12;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int number = 10;
+        //int number = 20;
+
+        //int uninitialized;
+        //System.out.println(uninitialized);
+
+        //String text = 123;
+
+        //System.out.println(undeclaredVariable);
+
+        //System.out.println(number);
+    }
+}
+
+// Ссылка (JavaRush): https://javarush.com/tasks/ru.javarush.java.core.level01.task12
+// Link (CodeGym): https://codegym.cc/tasks/en.codegym.java.core.level01.task12
